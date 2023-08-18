7d42885af1ed536f46fe13593b208367fda50aa0
Thomas67500 <thomas.vog67@gmail.com>
2023-08-18 22:01:52 +0200

    test

--- backend2/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String
-});
-
-const User = mongoose.model("User", UserSchema);
-
-module.exports = { User };
