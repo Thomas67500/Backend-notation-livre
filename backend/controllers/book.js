958c52a78bdde744ad12ab328e51bd2eeac1a549
Thomas67500 <thomas.vog67@gmail.com>
2023-08-21 17:29:39 +0200

    correction soutenance

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -14,7 +14,7 @@ exports.createBook = (req, res, next) => {
  
    book.save()
    .then(() => { res.status(201).json({message: 'Livre enregistré !'})})
-   .catch(error => { res.status(404).json( { error })})
+   .catch(error => { res.status(500).json( { error })})
 };
 
 exports.getOneBook = (req, res, next) => {
@@ -47,7 +47,7 @@ exports.getOneBook = (req, res, next) => {
           } else {
               Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
               .then(() => res.status(200).json({message : 'Livre modifié!'}))
-              .catch(error => res.status(403).json({ error }));
+              .catch(error => res.status(400).json({ error }));
           }
       })
       .catch((error) => {
@@ -65,7 +65,7 @@ exports.getOneBook = (req, res, next) => {
             fs.unlink(`images/${filename}`, () => {
                 Book.deleteOne({_id: req.params.id})
                     .then(() => { res.status(200).json({message: 'Livre supprimé !'})})
-                    .catch(error => res.status(403).json({ error }));
+                    .catch(error => res.status(400).json({ error }));
             });
         }
     })
